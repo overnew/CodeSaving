af3fe06f5c77c3bbb320f825d7a7672640144281
overnew <43613584+overnew@users.noreply.github.com>
2020-05-19 20:16:31 +0900

    Create 색종이만들기.cpp

---
+++ 색종이만들기.cpp
@@ -0,0 +1,62 @@
+//https://www.acmicpc.net/problem/2630
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int CutPaper(vector<vector<int>> paper, int& wn, int& bn,int n,int x, int y){
+    if(n ==1 ){
+        if(paper[x][y] == 0){
+            ++wn;
+            return 0;
+        }else{
+            ++bn;
+            return 1;
+        }
+    }
+
+    int div_n = n/2;
+    int area[4];
+    area[0] = CutPaper(paper, wn, bn, div_n, x, y);
+    area[1] = CutPaper(paper, wn, bn, div_n, x+div_n, y);
+    area[2] = CutPaper(paper, wn, bn, div_n, x, y+div_n);
+    area[3] = CutPaper(paper, wn, bn, div_n, x, y+div_n);
+
+    int sum = area[0] + area[1] +area[2] +area[3] ;
+
+    if(sum == 0){
+        wn -= 3;
+        return 0;
+    }else if(sum == 4){
+        bn -= 3;
+        return 1;
+    }
+    else{
+        return 5;
+    }
+     
+}
+
+
+int main(){
+    int n;
+    int t;
+    int wn=0;
+    int bn=0;
+    int x= 0, y=0;
+
+    vector<vector<int>> paper;
+    cin>>n;
+
+    for(int i=0 ; i< n ; ++i){
+        for(int j =0 ; j<n ; ++j){
+            cin>> t;
+            paper[i][j] =t;
+        }
+    }
+
+    CutPaper(paper, wn, bn, n, x, y);
+
+
+    return 0;
+}
