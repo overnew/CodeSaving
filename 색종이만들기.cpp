d3cda724ef69c825e90ae23a621ca9c0871ddd5d
overnew <43613584+overnew@users.noreply.github.com>
2020-05-20 19:50:07 +0900

    Update 색종이만들기.cpp

--- 색종이만들기.cpp
+++ 색종이만들기.cpp
@@ -1,4 +1,6 @@
 //https://www.acmicpc.net/problem/2630
+//군대에서 처음 푸는 문제이네, 이차원vector는 일단 1차원 vector 만든다음 넣어줘야함 
+//no.2630
 
 #include <iostream>
 #include <vector>
@@ -20,7 +22,7 @@ int CutPaper(vector<vector<int>> paper, int& wn, int& bn,int n,int x, int y){
     area[0] = CutPaper(paper, wn, bn, div_n, x, y);
     area[1] = CutPaper(paper, wn, bn, div_n, x+div_n, y);
     area[2] = CutPaper(paper, wn, bn, div_n, x, y+div_n);
-    area[3] = CutPaper(paper, wn, bn, div_n, x, y+div_n);
+    area[3] = CutPaper(paper, wn, bn, div_n, x+div_n, y+div_n);
 
     int sum = area[0] + area[1] +area[2] +area[3] ;
 
@@ -34,7 +36,7 @@ int CutPaper(vector<vector<int>> paper, int& wn, int& bn,int n,int x, int y){
     else{
         return 5;
     }
-     
+    
 }
 
 
@@ -46,16 +48,22 @@ int main(){
     int x= 0, y=0;
 
     vector<vector<int>> paper;
+    vector<int> temp;
     cin>>n;
 
     for(int i=0 ; i< n ; ++i){
         for(int j =0 ; j<n ; ++j){
             cin>> t;
-            paper[i][j] =t;
+            temp.push_back(t);
         }
+        paper.push_back(temp);
+        temp.clear();
     }
 
     CutPaper(paper, wn, bn, n, x, y);
+
+    cout<<wn<<endl;
+    cout<<bn<<endl;
 
 
     return 0;
