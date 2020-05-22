85a34d9c6af446a92ceb8699dffec4095a38c8cd
overnew <43613584+overnew@users.noreply.github.com>
2020-05-22 18:43:12 +0900

    Update 종이의개수.cpp

--- 종이의개수.cpp
+++ 종이의개수.cpp
@@ -1,36 +1,15 @@
 //https://www.acmicpc.net/problem/1780
+// 맞는 풀이 같은데 시간초과.. 전역변수를 쓰거나 덧셈 뺄셈을 좀 
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
+int minus_n =0,plus_n =0, zero_n=0 ;
 
-int main(){
-  int n;
-  int t;
-  int minus_n=0, plus_n=0, zero_n=0;
-  int x=0,y=0;
-  vector<vector<int>> paper;
-  vector<int> temp;
-  cin>>n;
 
-  for(int i=0; i<n ; ++i){
-    for(int j=0; j<n ; ++j ){
-      cin>>t;
-      temp.push_back(t);
-    }
-    paper.push_back(temp);
-    temp.clear();
-  }
+int CountPaper(const vector<vector<int>> paper, int n, int x, int y){
 
-  cout<< minus_n<<endl;
-  cout<< zero_n<<endl;
-  cout<< plus_n<<endl;
-
-  return 0;
-}
-
-int CountPaper(const vector<vector<int>> paper, int minus_n, int plus_n, int zero_n,int n, int x, int y){
   if(n == 1){
     if(paper[x][y] == -1){
       ++minus_n;
@@ -45,31 +24,34 @@ int CountPaper(const vector<vector<int>> paper, int minus_n, int plus_n, int zer
   }
 
   int div_n = n/3;
-  int area[9];
-  area[0] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x,y );
-  area[1] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x+div_n,y );
-  area[2] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x + (div_n*2),y );
-  area[3] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x,y+div_n );
-  area[4] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x+div_n,y+div_n );
-  area[5] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x+ (div_n*2),y+div_n );
-  area[6] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x,y + (div_n*2));
-  area[7] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x+div_n,y + (div_n*2));
-  area[8] = CountPaper(paper, minus_n,plus_n ,zero_n, div_n, x+ (div_n*2),y + (div_n*2));
-  
-  int m_c,z_c,p_c,t_c, is2 =0;
+  int div2 = div_n *2;
 
-  for(int i=0 ; i<n ;++i){
+  int area[9];
+  area[0] = CountPaper(paper,  div_n, x,y );
+  area[1] = CountPaper(paper,  div_n, x+div_n,y );
+  area[2] = CountPaper(paper,  div_n, x + div2,y );
+  area[3] = CountPaper(paper,  div_n, x,y+div_n );
+  area[4] = CountPaper(paper,  div_n, x +div_n, y+div_n );
+  area[5] = CountPaper(paper,  div_n, x + div2, y+div_n );
+  area[6] = CountPaper(paper,  div_n, x, y + div2);
+  area[7] = CountPaper(paper,  div_n, x+div_n, y + div2);
+  area[8] = CountPaper(paper,  div_n, x+ div2, y + div2);
+  
+  int m_c=0,z_c =0,p_c=0,t_c=0, is2 =0;
+
+  for(int i=0 ; i<9 ;++i){
     if(area[i] == -1) ++m_c;
     else if(area[i] == 0) ++z_c;
     else if(area[i] == 1) ++p_c;
     else ++t_c;
   }
 
-  if(t_c != 0) return 2;
-  if(m_c !=0) ++is2;
-  if(z_c!=0) ++is2;
+  if(t_c != 0) return 2; // 2가 반환된것 이 있다면 바로 다시 2반환
+
+  if(m_c != 0) ++is2;
+  if(z_c!= 0) ++is2;
   if(p_c!=0) ++is2;
-  
+
   if(is2==1){
     if(m_c !=0){
       minus_n -= 8;
@@ -80,7 +62,33 @@ int CountPaper(const vector<vector<int>> paper, int minus_n, int plus_n, int zer
     }else{
       plus_n -= 8;
       return 1;
+    }
+   // 위에서 안걸리면 2인것
   }
-  return 2; // 위에서 안걸리면 2인것
+  return 2;
 }
+
+int main(){
+  int n;
+  int t;
+  vector<vector<int>> paper;
+  vector<int> temp;
+  cin>>n;
+
+  for(int i=0; i<n ; ++i){
+    for(int j=0; j<n ; ++j ){
+      cin>>t;
+      temp.push_back(t);
+    }
+    paper.push_back(temp);
+    temp.clear();
+  }
+
+  CountPaper(paper, n, 0,0 );
+
+  cout<< minus_n<<endl;
+  cout<< zero_n<<endl;
+  cout<< plus_n<<endl;
+
+  return 0;
 }
