cc620bc17a475acd89e558a8074a9f764704c86e
overnew <43613584+overnew@users.noreply.github.com>
2020-07-10 17:46:37 +0900

    Create 부분합_부분합.cpp

---
+++ 부분합_부분합.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/1806
+
+#include<iostream>
+using namespace std;
+
+int sump[100000];
+
+int main() {
+	int n,s;
+	cin>>n >> s;
+
+	sump[0] = 0; //부분합 계산에 용이 하도록! 배열의 첫 값 =sump[1] -sump[0]
+	for (int i=1; i<=n ; ++i) {
+		cin >> sump[i];
+		sump[i] += sump[i - 1];
+	}
+
+	for (int i=1; i<=n ; ++i) { //i는 배열의 길이
+		for (int j=i ; j<=n ; ++j) {
+			if (sump[j] - sump[j - i] >= s) {
+				cout << i << endl;
+				return 0;
+			}
+		}
+	}
+
+	cout << 0 << endl;
+
+	return 0;
+}
