bddf0462f62eeb22035d26fc9facae6e1065e282
overnew <43613584+overnew@users.noreply.github.com>
2020-07-24 23:17:56 +0900

    Update Tree_이진 검색 트리.cpp

--- Tree_이진 검색 트리.cpp
+++ Tree_이진 검색 트리.cpp
@@ -4,6 +4,93 @@ https://www.acmicpc.net/problem/5639
 #include<vector>
 using namespace std;
 
+class BinaryTree{
+  public:
+  int value;
+  BinaryTree* left;
+  BinaryTree* right;
+
+  BinaryTree(int v,BinaryTree* l =NULL, BinaryTree* r = NULL){
+    this->left = l;
+    this->right = r;
+    this->value =v;
+  }
+};
+
+class Tree{
+  public:
+  BinaryTree* root;
+  BinaryTree* current;
+  BinaryTree* parent;
+  vector<BinaryTree> v;
+
+  Tree(int n){
+    v.push_back(BinaryTree(n));
+    root = &v[0];
+  }
+
+  void AddNode(int n){
+    v.push_back(BinaryTree(n));
+    current = root;
+
+    while(1){
+      if(n < current->value){
+        if(current->left == NULL){
+          current->left = &v.back();
+          break;
+        }else
+          current = current->left;
+      }else{
+        if(current->right == NULL){
+          current->right =  &v.back();
+          break;
+        }else
+          current = current->right;
+      }
+    }
+  }
+
+  void PostOrder(BinaryTree* c){
+    if(c->left !=NULL)
+      PostOrder(c->left);
+    if(c->right !=NULL)
+      PostOrder(c->right);
+    cout<<c->value<<'\n';
+  }
+
+};
+
+
+int main() {
+  int n;
+  cin>>n;
+  Tree bin(n);
+  ios_base::sync_with_stdio(0);
+
+  while(1){
+    cin>>n;
+    if(n==-1)
+      break;
+    bin.AddNode(n);
+  }
+
+  for(int i=0; i<bin.v.size() ; ++i){
+    cout<<bin.v[i].value<<": ";
+    if(bin.v[i].left != NULL)
+      cout<<bin.v[i].left->value<<", ";
+    if(bin.v[i].right != NULL)
+      cout<<bin.v[i].right->value;
+    cout<<'\n';
+  }
+  bin.PostOrder(bin.root);
+
+  return 0;
+}
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
 typedef struct bin{
   int value;
   bin* left;
