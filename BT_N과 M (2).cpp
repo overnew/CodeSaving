565ad0c4b0dff8e852d7c1fdb05f5fac55b33540
EVEerNew <43613584+overnew@users.noreply.github.com>
2020-08-25 22:19:03 +0900

    Create BT_N과 M (2).cpp

---
+++ BT_N과 M (2).cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/15650
+
+#include <iostream>
+using namespace std;
+
+int n,m;
+int arr[9];
+
+void Recur(int cnt, int idx){
+  if(cnt == m){
+    for(int i=0; i<m ; ++i){
+      cout<<arr[i]<<' ';
+    }
+    cout<<'\n';
+  }
+
+  for(int i=idx; i<=n ; ++i ){
+    arr[cnt] = i;
+    Recur(cnt+1, i+1);
+  }
+
+}
+
+int main() {
+  cin>> n>>m;
+
+  Recur(0, 1); //idx는 1부터 시작해야함
+
+  return 0;
+}
