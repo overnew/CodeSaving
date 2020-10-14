bb6a5f3935ddd2bbcd6be35faa2e6881f3a635ad
EVEerNew <43613584+overnew@users.noreply.github.com>
2020-10-14 19:05:34 +0900

    Update and rename boggleTemp.cpp to DP_Boggle.cpp
    이름과 코드변경

--- DP_Boggle.cpp
+++ DP_Boggle.cpp
@@ -1,3 +1,5 @@
+//https://www.algospot.com/judge/problem/read/BOGGLE
+
 #include<iostream>
 #include<string>
 #include<string.h>
@@ -10,91 +12,19 @@ string words[10];
 bool isPosWords[10];
 string wordNow;
 int lenOfNowWord; 
-
-void CheckPosAlp(int _r,int _c){
-  int r,c;
-  for(int i=0 ; i<8 ; ++i){
-    r= _r;
-    c=_c;
-    switch(i){
-      case 0: //왼쪽 위부터 시계 방향으로
-        --r;
-        --c;
-      break;
-      case 1:
-        --r;
-      break;
-      case 2:
-        --r;
-        ++c;
-      break;
-      case 3:
-        ++c;
-      break;
-      case 4:
-        ++r;
-        ++c;
-      break;
-      case 5:
-        ++r;
-      break;
-      case 6:
-        ++r;
-        --c;
-      break;
-      case 7:
-        --c;
-      break;
-    }
-
-    if(0<=r && r<5 && 0<=c && c<5){
-      posAlphabet[_r][_c][board[r][c]-'A'] =true; //
-    }
-  }
-
-}
-
+int changeRow[8] ={-1,-1,-1,0,1,1,1,0};    //왼쪽 위부터 시계 방향으로
+int changeCol[8] = {-1,0,1,1,1,0,-1,-1};
 
 bool FindWordRec(int _r,int _c, int idxOfChar){
-  if(idxOfChar ==lenOfNowWord)
-    return true;
+  if(idxOfChar ==lenOfNowWord)  //단어의 끝에 도달하면 성공
+    return true;  
 
   int r,c;
 
   for(int i=0 ; i<8 ; ++i){
-    r= _r;
-    c=_c;
-    switch(i){
-      case 0: //왼쪽 위부터 시계 방향으로
-        --r;
-        --c;
-      break;
-      case 1:
-        --r;
-      break;
-      case 2:
-        --r;
-        ++c;
-      break;
-      case 3:
-        ++c;
-      break;
-      case 4:
-        ++r;
-        ++c;
-      break;
-      case 5:
-        ++r;
-      break;
-      case 6:
-        ++r;
-        --c;
-      break;
-      case 7:
-        --c;
-      break;
-    }
-
+    r= _r + changeRow[i];
+    c= _c + changeCol[i];
+ 
     if(0<=r && r<5 && 0<=c && c<5){
       if(board[r][c] ==  wordNow[idxOfChar] &&posAlphabet[r][c][idxOfChar]){
         posAlphabet[r][c][idxOfChar] =FindWordRec(r, c, idxOfChar +1);
@@ -138,8 +68,9 @@ int main(){
 
       for(int r=0; r<5 ;++r ){
         for(int c=0; c<5 ; ++c)
-          if(FindWordRec(r, c, 0))
-            isPosWords[j]=true;
+          if(board[r][c] == wordNow[0])   //첫 글자가 맞는 경우에만 호출
+            if(FindWordRec(r, c, 1))      //첫 글자는 맞기 때문에 1부터 탐색
+              isPosWords[j]=true;
       }
 
     }
